64410dcefefbd06d4808bcf854164407282b51c1
Riveascore <ChrisAga.dev@gmail.com>
2015-07-26 15:28:50 -0500

    Check for new reminders script now posts new reminders to HabitRPG profile

--- scripts/check_for_new_reminders.rb
+++ scripts/check_for_new_reminders.rb
@@ -1,4 +1,5 @@
 require 'lib/handle_icloud_reminders'
+require 'lib/handle_habit_rpg'
 require 'lib/tick_every'
 
 # Check for new reminder every 20 seconds
@@ -16,7 +17,13 @@ thread = tick_every(time_interval) do
   if reminders.size > current_reminders_size
     current_reminders_size = reminders.size
     sorted_reminders = reminders.sort_by { |rem| rem.created_date_extended }
-    new_reminder = sorted_reminders.last
-    puts "Title: #{new_reminder.title}"
+    @new_reminder = sorted_reminders.last
+    puts "Title: #{@new_reminder.title}"
+
+    # Now! Add to HabitRPG
+    task_options = { :text => @new_reminder.title }
+    HandleHabitRPG.add_task(task_options)
+    # Now! Add to HabitRPG
+
   end
 end
