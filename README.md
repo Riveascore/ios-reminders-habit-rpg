d19becdc965209c4a2af04aeffed8b7c348881a9
Riveascore <ChrisAga.dev@gmail.com>
2015-07-26 14:45:21 -0500

    Created handle_icloud_reminders module, updated instructions

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ A project to help iOS Reminders app and HabitRPG interact and update one another
 
 ### Setup:
 - Clone `ruby-icloud` in [first step according](https://github.com/adammck/ruby-icloud#try-it)
-- Copy `/lib/icloud` to this project's `/lib`
+- Copy **everything** in `/lib` to this project's `/lib`
 - Make sure to add the follwing **environment variables** to `~/.bashrc`
   - HABIT_RPG_USER_ID
   - HABIT_RPG_API_KEY
---
+++ lib/handle_icloud_reminders.rb
@@ -0,0 +1,23 @@
+require 'icloud'
+
+module HandleICloudReminders
+
+	# iCloud username and password from environment variables
+	ICLOUD_USERNAME = ENV['ICLOUD_USERNAME']
+	ICLOUD_PASSWORD = ENV['ICLOUD_PASSWORD']
+
+	def self.add_reminder(options = {})
+		options = {
+			:title => 'Default handle_ios_reminders'
+		}.merge(options)
+
+		session = ICloud::Session.new(ICLOUD_USERNAME, ICLOUD_PASSWORD)
+
+		reminders = session.reminders
+
+		new_reminder = ICloud::Records::Reminder.new
+		new_reminder.title = options[:title]
+
+		session.post_reminder(new_reminder)
+	end
+end
