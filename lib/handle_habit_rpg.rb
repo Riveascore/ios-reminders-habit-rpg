505d2befb28a655d259d90cf000c3dae2a93569e
Riveascore <ChrisAga.dev@gmail.com>
2015-07-26 14:47:56 -0500

    Use spaces instead of tabs for indentation

--- lib/handle_habit_rpg.rb
+++ lib/handle_habit_rpg.rb
@@ -3,44 +3,44 @@ require 'curb'
 
 module HandleHabitRPG
 
-	# Key and user_id are static in environment
-	API_KEY = ENV['HABIT_RPG_API_KEY']
-	HABIT_RPG_USER_ID = ENV['HABIT_RPG_USER_ID']
+  # Key and user_id are static in environment
+  API_KEY = ENV['HABIT_RPG_API_KEY']
+  HABIT_RPG_USER_ID = ENV['HABIT_RPG_USER_ID']
 
-	# Curl headers for http get/post, always interact via JSON
-	HEADERS = {
-		'Content-Type' => 'application/json',
-		'Accept' => 'application/json',
-		'x-api-user' => HABIT_RPG_USER_ID,
-		'x-api-key' => API_KEY
-	}
+  # Curl headers for http get/post, always interact via JSON
+  HEADERS = {
+    'Content-Type' => 'application/json',
+    'Accept' => 'application/json',
+    'x-api-user' => HABIT_RPG_USER_ID,
+    'x-api-key' => API_KEY
+  }
 
-	# Base api url along with hash of HabitRPG API urls we can call
-	API_PREFIX = "https://habitrpg.com:443/api/v2"
-	SWAGGER_URLS = {
-		:tasks => {
-			:get => "/user/tasks",
-			:create => "/user/tasks"
-		}
-	}
+  # Base api url along with hash of HabitRPG API urls we can call
+  API_PREFIX = "https://habitrpg.com:443/api/v2"
+  SWAGGER_URLS = {
+    :tasks => {
+      :get => "/user/tasks",
+      :create => "/user/tasks"
+    }
+  }
 
-	def self.make_url(url_suffix)
-		return "#{API_PREFIX}#{url_suffix}"
-	end
+  def self.make_url(url_suffix)
+    return "#{API_PREFIX}#{url_suffix}"
+  end
 
-	def self.add_task(options = {})
-		options = {
-			:type => 	'todo',
-			:text => 'New todo from ruby!'
-		}.merge(options)
+  def self.add_task(options = {})
+    options = {
+      :type =>  'todo',
+      :text => 'New todo from ruby!'
+    }.merge(options)
 
-		payload = options.to_json
-		
-		curl = Curl::Easy.new
-		create_task_url = make_url(SWAGGER_URLS[:tasks][:get])
+    payload = options.to_json
+    
+    curl = Curl::Easy.new
+    create_task_url = make_url(SWAGGER_URLS[:tasks][:get])
 
-		curl.url = create_task_url
-		curl.headers = HEADERS
-		curl.http_post(payload)
-	end
+    curl.url = create_task_url
+    curl.headers = HEADERS
+    curl.http_post(payload)
+  end
 end
--- lib/handle_icloud_reminders.rb
+++ lib/handle_icloud_reminders.rb
@@ -2,22 +2,22 @@ require 'icloud'
 
 module HandleICloudReminders
 
-	# iCloud username and password from environment variables
-	ICLOUD_USERNAME = ENV['ICLOUD_USERNAME']
-	ICLOUD_PASSWORD = ENV['ICLOUD_PASSWORD']
+  # iCloud username and password from environment variables
+  ICLOUD_USERNAME = ENV['ICLOUD_USERNAME']
+  ICLOUD_PASSWORD = ENV['ICLOUD_PASSWORD']
 
-	def self.add_reminder(options = {})
-		options = {
-			:title => 'Default handle_ios_reminders'
-		}.merge(options)
+  def self.add_reminder(options = {})
+    options = {
+      :title => 'Default handle_ios_reminders'
+    }.merge(options)
 
-		session = ICloud::Session.new(ICLOUD_USERNAME, ICLOUD_PASSWORD)
+    session = ICloud::Session.new(ICLOUD_USERNAME, ICLOUD_PASSWORD)
 
-		reminders = session.reminders
+    reminders = session.reminders
 
-		new_reminder = ICloud::Records::Reminder.new
-		new_reminder.title = options[:title]
+    new_reminder = ICloud::Records::Reminder.new
+    new_reminder.title = options[:title]
 
-		session.post_reminder(new_reminder)
-	end
+    session.post_reminder(new_reminder)
+  end
 end
