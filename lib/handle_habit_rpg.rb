47d40b74b4ad21504b181cba7c877d8a30139d0b
Riveascore <ChrisAga.dev@gmail.com>
2015-07-26 14:04:44 -0500

    Moved `add_new_task` to module and created more generic setup
    Also, saving `current_reminders_size` after finding new iOS reminder.

---
+++ lib/handle_habit_rpg.rb
@@ -0,0 +1,46 @@
+require "json_serializer"
+require 'curb'
+
+module HandleHabitRpg
+
+	# Key and user_id are static in environment
+	API_KEY = ENV['HABIT_RPG_API_KEY']
+	HABIT_RPG_USER_ID = ENV['HABIT_RPG_USER_ID']
+
+	# Curl headers for http get/post, always interact via JSON
+	HEADERS = {
+		'Content-Type' => 'application/json',
+		'Accept' => 'application/json',
+		'x-api-user' => HABIT_RPG_USER_ID,
+		'x-api-key' => API_KEY
+	}
+
+	# Base api url along with hash of HabitRPG API urls we can call
+	API_PREFIX = "https://habitrpg.com:443/api/v2"
+	SWAGGER_URLS = {
+		:tasks => {
+			:get => "/user/tasks",
+			:create => "/user/tasks"
+		}
+	}
+
+	def make_url(url_suffix)
+		return "#{API_PREFIX}#{url_suffix}"
+	end
+
+	def add_todo(options = {})
+		options = {
+			:type => 	'todo',
+			:text => 'New todo from ruby!'
+		}.merge(options)
+
+		payload = options.to_json
+		
+		curl = Curl::Easy.new
+		create_task_url = make_url(SWAGGER_URLS[:tasks][:get])
+
+		curl.url = create_task_url
+		curl.headers = HEADERS
+		curl.http_post(payload)
+	end
+end
--- scripts/check_for_new_reminders.rb
+++ scripts/check_for_new_reminders.rb
@@ -12,13 +12,14 @@ end
 # Time in seconds
 time_interval = 20
 initial_session = ICloud::Session.new("riveascorehabitrpg@gmail.com", "Gonna get some shit done1")
-initial_reminders_size = initial_session.reminders.size
+current_reminders_size = initial_session.reminders.size
 
 thread = tick_every(time_interval) do
 	# Have to get new session every time
   session = ICloud::Session.new("riveascorehabitrpg@gmail.com", "Gonna get some shit done1")
   reminders = session.reminders
-  if reminders.size > initial_reminders_size
+  if reminders.size > current_reminders_size
+    current_reminders_size = reminders.size
   	sorted_reminders = reminders.sort_by { |rem| rem.created_date_extended }
   	@new_reminder = sorted_reminders.last
   	puts "Title: #{@new_reminder.title}"
